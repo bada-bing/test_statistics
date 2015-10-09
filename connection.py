c2eb3651c0fc6b3da907d45d95e3cb3ed9aeea42
Milan <bada.bing.bing@live.com>
2015-10-10 00:35:05 +0200

    Changes to be committed: 	modified:   connection.py 	modified:   main.py

--- connection.py
+++ connection.py
@@ -3,33 +3,34 @@ __author__ = 'Milan'
 import mysql.connector
 from mysql.connector import errorcode
 
-try:
-  db = mysql.connector.connect(
-      host="localhost",
-      user="root",
-      passwd="",
-      db="test_db"
-  )
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-  db.close()
-else:
-    cursor = db.cursor()
+def connect():
+    try:
+      db = mysql.connector.connect(
+          host="localhost",
+          user="root",
+          passwd="",
+          db="test_db"
+      )
+    except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+      elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+      else:
+        print(err)
+      db.close()
+    else:
+        cursor = db.cursor()
 
-    add = ("INSERT INTO table_test "
-               "VALUES (%s, %s)")
+        add = ("INSERT INTO table_test "
+                   "VALUES (%s, %s)")
 
-    data = (6112, 'mm')
+        data = (6115, 's')
 
-    cursor.execute(add, data)
+        cursor.execute(add, data)
 
-    db.commit()
+        db.commit()
 
-    cursor.close()
+        cursor.close()
 
-    db.close()
+        db.close()
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 __author__ = 'Milan'
 
 import odbchelper
-
+import connection
 import sys
 
 #print(odbchelper.buildConnectionString.__doc__)
 
-print(sys.path)
+#print(sys.path)
 
 (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) = range(7)
-print(Tuesday)
+
+print("Danas je " + str(Tuesday))
+
+connection.connect()
+
+
+
